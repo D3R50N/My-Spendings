ddc6c32ffd461224985df2b692b5a896d0a83924
HappyDev <67974781+D3R50N@users.noreply.github.com>
2022-09-09 16:08:59 +0100

    un commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ Future<void> main() async {
   prefs = await SharedPreferences.getInstance();
   runApp(const App());
 } 
+
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
